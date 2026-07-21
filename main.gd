1f979cc9297e07a49da1f410d419b4d675e8177b
Jenifer <yuweicheng20@gmail.com>
2026-07-21 11:55:41 -0400

    HUD scene

--- main.gd
+++ main.gd
@@ -4,16 +4,22 @@ extends Node
 var score
 
 func _ready():
-	new_game()
+	pass
 
 func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 	
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	
+	get_tree().call_group("mobs", "queue_free")
 
 
 func _on_mob_timer_timeout() -> void:
@@ -43,6 +49,7 @@ func _on_mob_timer_timeout() -> void:
 
 func _on_score_timer_timeout() -> void:
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_start_timer_timeout() -> void:
